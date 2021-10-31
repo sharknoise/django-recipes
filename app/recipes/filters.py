3d099c0df466e39afe6e358bdff2b0754b5b7d0a
sharknoise <sharknoise@gmail.com>
2021-10-31 22:32:07 +0300

    add filters

---
+++ app/recipes/filters.py
@@ -0,0 +1,25 @@
+import django_filters
+from django.db import models
+from recipes.models import Ingredient, Recipe
+
+
+class RecipeFilter(django_filters.FilterSet):
+    #recipes = django_filters.ModelChoiceFilter(
+    #    queryset=Recipe.objects.all()
+    #)
+
+    ingredients = django_filters.ModelChoiceFilter(
+        queryset=Ingredient.objects.all()
+    )
+    class Meta:
+        model = Recipe
+        fields = ['name']
+
+        filter_overrides = {
+            models.CharField: {
+                'filter_class': django_filters.CharFilter,
+                'extra': lambda f: {
+                    'lookup_expr': 'icontains',
+                },
+            },
+        }
--- app/recipes/views.py
+++ app/recipes/views.py
@@ -1,7 +1,9 @@
-from django.views.generic import ListView
+from django_filters.views import FilterView
+from recipes.filters import RecipeFilter
 from recipes.models import Recipe
 
 
-class RecipesListView(ListView):
+class RecipesListView(FilterView):
     model = Recipe
     template_name = 'recipes-list.html'
+    filterset_class = RecipeFilter
